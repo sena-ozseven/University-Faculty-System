5d297f8c5d72b75915eb84dae3455f5d8b130862
sena <gulozseven@gmail.com>
2025-07-29 20:08:07 +0300

    Update Faculty.java

--- src/Faculty.java
+++ src/Faculty.java
@@ -1,11 +1,19 @@
 public class Faculty {
+    private String name;
     private University university;
 
-    public  Faculty(University university) {
+    public  Faculty(String name, University university) {
+        this.name = name;
         this.university = university;
+        this.university.addFaculty(this); //Trick: add the faculty to the university that is sent.
+                                         //note that this may not be the desired thing to do for all cases.
     }
 
     public University getUniversity() {
         return university;
+    }
+
+    public String getName() {
+        return name;
     }
 }
