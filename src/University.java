a233701f23f3d05fdbddf89384c6d95075f6b4fd
sena <gulozseven@gmail.com>
2025-07-29 19:11:37 +0300

    Update University.java

--- src/University.java
+++ src/University.java
@@ -1,19 +1,30 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class University {
 
     // 1. These are called "FIELDS"
+    private Long id;
     private final String name;  // 1.1 Final fields cannot have setter methods.
     private List<Faculty> faculties;
 
 
     // 2. Since 'name' is final, we must initialize it through a constructor.
-    public University(String name) {
+    public University(Long id, String name) {
+        this.id = id;
+
+                // if (name == null || name.isBlank() || name.isEmpty()) {
+                    //throw new IllegalArgumentException("Name cannot be null, empty or blank.");
+                // }
+                //--> this is against to the single responsibility principle.
+                // Meaning, this is not a responsibility for the University class.
+                //To make this control, we create a new java class named "Utility" or "Validator".
+
         this.name = name;
         // 2.1 Initialize faculties as an empty list to avoid null pointer issues later.
-        this.faculties = new ArrayList<>();
+        this.faculties = new ArrayList<>(); //Defensive copy
 
     }
     //when we want to call this class, we have to give a name to the constructor
@@ -22,8 +33,8 @@ public class University {
     // bc we do not have a constructor like this anymore.
 
     //1.5 -  2nd constructor -w/ OVERLOADING
-    public University(String name, List<Faculty> faculties) {
-        this(name);  // 3.1 Calls the other constructor to set the 'name'.
+    public University(Long id, String name, List<Faculty> faculties) {
+        this(id, name);  //  Constructor chaining
 
         //we do NOT want to write:
         //this.faculties = faculties
@@ -46,6 +57,10 @@ public class University {
         return this.name;
     }
 
+    public Long getId() {
+        return this.id;
+    }
+
     // 5. Important Note: Avoid exposing mutable lists directly via getter.
     // 5.1 For example, this would allow external modification:
     //
@@ -61,6 +76,7 @@ public class University {
     //     - Return an unmodifiable copy      --> e.g. Collections.unmodifiableList(this.faculties) -- read-only
     //     - Or return a deep copy if needed. --> e.g. return new ArrayList<>(this.faculties)
 
+    //Thus, this is the best practice of the encapsulation: -- defensive copy
     public List<Faculty> getFaculties() {
         return Collections.unmodifiableList(this.faculties);
     }
@@ -73,6 +89,27 @@ public class University {
 
     public void removeFaculty(Faculty faculty) {
         this.faculties.remove(faculty);
+    }
+
+    @Override
+    public String toString() {
+        return "University name:  " + this.name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {                                                                             // this      ==    (obj)
+        if (obj == this) { //if the coming obj's reference is equals to "this" (=this.university), return true. ==> workintech.equals(workintech)
+            return true;
+        }
+        if (obj == null || obj.getClass() != this.getClass()) {
+            return false;
+        }
+        University university = (University) obj; //--> DOWNCASTING
+        return university.getId().equals(this.id ); //-------
+    }                                                        //
+    @Override                                               // these two must check the same fields -- in this case it is id.
+    public int hashCode() {                                //
+        return Objects.hash(this.id); //----------
     }
 
 }
